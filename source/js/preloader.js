088637430497ab5653d30b18e71fb6a3c312c2a5
DenisSeroshtan <haron_777@mail.ru>
2017-12-20 00:11:58 +0500

    header top

---
+++ source/js/preloader.js
@@ -0,0 +1,11 @@
+//preloader
+window.onload = function() {
+  document.querySelector('.wrapper').classList.add('loaded');
+};
+$(function () {
+  $('.js-lastWord').each(function() {
+    var $this = $(this);
+    // добавляем к последнему слову в выражении span
+    $this.html($this.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+  });
+})
