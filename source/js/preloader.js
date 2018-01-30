51620f15f77a06d0306cd2f7f8df57090fdfaf17
DenisSeroshtan <haron_777@mail.ru>
2018-01-31 03:04:58 +0500

    add section blog and js block hidden

--- source/js/preloader.js
+++ source/js/preloader.js
@@ -8,6 +8,11 @@ $(function () {
     // добавляем к последнему слову в выражении span
     $this.html($this.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
   });
+  $('.js-firstWord').each(function() {
+    var $this = $(this);
+    // добавляем к первому слову в выражении span
+    $this.html($this.html().replace(/^(\S+)/, '<span>$1</span>'));
+  });
 
   // стилизация select
   $("#js-select").SumoSelect({
@@ -25,3 +30,29 @@ $(function () {
     });
   })();
 });
+
+// hidden text in section blog
+(function(){
+  $('.post-link__hide').on('click', function (e) {
+    e.preventDefault();
+    var
+        $this = $(this),
+        container = $this.closest('.post-content'),
+        contentShow = container.find('.js-content__show'),
+        contentShowText = contentShow.text(),
+        textSpace = contentShowText.replace( /\.{3,}$/, ' '),
+        textDots = contentShowText.replace(/ $/, '...'),
+        contentHide = container.find('.js-content__hide');
+
+    contentHide.toggleClass("hidden");
+
+    if (!contentHide.hasClass('hidden')) {
+      $this.text('Hide')
+      contentShow.text(textSpace);
+    }else {
+      $this.text('Read more')
+      contentShow.text(textDots);
+    }
+  })
+
+})();
