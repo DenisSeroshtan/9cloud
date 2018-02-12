9674760924f2e9c31c8511ba7aa4532d08c62b27
DenisSeroshtan <haron_777@mail.ru>
2018-02-12 20:56:18 +0500

    add mobile menu

--- source/js/preloader.js
+++ source/js/preloader.js
@@ -12,12 +12,19 @@ $(function () {
     $this.html($this.html().replace(/^(\S+)/, '<span>$1</span>'));
   });
 
-  // стилизация select
+  /*=================
+      sumoselect
+      https://github.com/HemantNegi/jquery.sumoselect.git
+  =================*/
+
   $("#js-select").SumoSelect({
     search: true,
     searchText: 'Search'
   });
-
+/*=================
+    slider slick
+    http://kenwheeler.github.io/slick/
+=================*/
   (function () {
     $('.slider__list').slick({
       arrows : false,
@@ -54,7 +61,9 @@ $(function () {
   })
 
 })();
-
+/*=================
+    smooth scroll
+=================*/
 (function () {
 
   $('a[href^="#js"]').on('click', function(e) {
@@ -64,22 +73,30 @@ $(function () {
           $this = $(this),
           target = $this.attr('href'),
           hTarget = $(target).offset().top,
-          duration = 1500;
+          duration = 600;
 
       $('html,body').animate({
-          scrollTop : hTarget
-      }, duration)
+          scrollTop : hTarget - 30
+      }, duration, function() {
+        if($('.nav').css('display') === 'block') {
+          $(".nav").slideUp(100, function () {
+            $('.hamburger').removeClass('is-active');
+            $(this).removeAttr('style');
+          });
+        }
+      });
 
   });
 
 })();
-
+/*=================
+    fixed menu to scroll
+=================*/
 (function() {
-  // fixed menu to scroll
   var
       header = $('.header'),
       hHeader = header.outerHeight(),
-      nav = $('.header-top'),
+      nav = $('.header-top__wrap'),
       hNav = nav.outerHeight();
 
   $(document).on('scroll', function(e) {
@@ -95,6 +112,74 @@ $(function () {
       header.removeAttr('style');
     }
 
+  });
+})();
+/*=================
+ highlighting the active menu item
+=================*/
+(function(){
+  function checkSection() {
+    $('header, section[id^=js]').each(function () {
+      var
+          $this = $(this),
+          topEdge = $this.offset().top - 50,
+          bottomEdge = topEdge + $this.height(),
+          wScroll = $(window).scrollTop(),
+          currentId = $this.attr('id'),
+          reqLink = $('.nav__item-link').filter('[href="#' +currentId +'"]');
+
+
+      if (topEdge < wScroll && bottomEdge > wScroll) {
+        reqLink.closest(".nav__item").addClass('active')
+            .siblings().removeClass('active')
+      }
+    });
+  };
+
+  $(document).on('scroll', checkSection);
+})();
+
+/*=================
+    hamburger menu
+=================*/
+(function () {
+  $('.hamburger').on('click', function(e) {
+      e.preventDefault();
+
+      var
+          $this = $(this),
+          menu = $this.attr('href');
+
+      if (!$this.hasClass('is-active')) {
+        $(menu).slideDown();
+        $this.addClass('is-active');
+      } else {
+        $(menu).slideUp(400, function() {
+          $(this).removeAttr('style')
+        })
+        $this.removeClass('is-active');
+      }
+  });
+  /*=================
+      close menu with click document
+  =================*/
+  $(document).on('click', function(e) {
+    var $this = $(e.target);
+    var wWidth = $(window).outerWidth();
+
+    var
+        slideMenu = $('.nav'),
+        container = slideMenu.closest('.header-top__wrap'),
+        hamburger = container.find('.hamburger');
+    if (!$this.closest('.header-top__wrap').length && wWidth <= 900) {
+      slideMenu.slideUp(400, function (){
+        var $this = $(this);
+        if ($this.css('display') === 'none'){
+          $this.removeAttr('style');
+        }
+      });
+      hamburger.removeClass('is-active');
+    }
   });
 })();
 
